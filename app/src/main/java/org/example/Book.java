355d15067a20a46a33ec20f1ad18496f79586439
JoelAdj001 <kofienergy15@gmail.com>
2024-12-12 12:25:46 +0000

    Made new modification

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,25 @@
 public class Book {
+    private String bookTitle;
+    private String bookAuthor;
+    private int yearPublish;
 
+    public Book(Sting bookTitle, String bookAuthor, int yearPublish){
+        this.booTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.yearPublish = yearPublish;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public int getYearPublish() {
+        return yearPublish;
+    }
     // This is your working class.
     
 }
